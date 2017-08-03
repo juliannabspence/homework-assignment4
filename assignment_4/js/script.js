5f6a850089047c3d5a1844f1301d0c53f7f7765f
juliannabspence <julianna.spence@gmail.com>
2017-08-02 18:07:21 -0700

    final homework submission

--- assignment_4/js/script.js
+++ assignment_4/js/script.js
@@ -14,34 +14,46 @@ $("p#second-read-more-text").hide();
 // Click "read more" - #first-read-more-button
 $("#first-read-more-button").click(slideDownText);
 $("#first-read-less-button").click(slideUpText);
-
+$("#second-read-more-button").click(slideDownText2);
+$("#second-read-less-button").click(slideUpText2);
 
 function slideDownText() {
 	//expands texts and - #first-read-more-text
 	 $("#first-read-more-text").slideDown();
 		$("#first-read-more-button").hide();
 	    $("#first-read-less-button").show();
+	    event.preventDefault();
+
+}	   
 
 function slideUpText() {
 	//hides texts and - #first-read-less-text
-	$("#first-read-less-text").slideUp();
+	$("#first-read-more-text").slideUp();
 		$("#first-read-more-button").show();
 		$("#first-read-less-button").hide();
+		event.preventDefault();
+
 } 	    
 
 // up until here it functions alright - but it expands the second-read-more-text when I click "read more" on the first-paragraph
+// second paragraph
 
-$("#second-read-more-button").click(slideDownText);
-$("#second-read-less-button").click(slideUpText);
-	// second paragraph
+
+
+function slideDownText2() {
 	 $("#second-read-more-text").slideDown();
 	    $("#second-read-more-button").hide();
 	    $("#second-read-less-button").show();   
+	    event.preventDefault();
 }
 
-	$("#second-read-less-text").slideUp();
+function slideUpText2() {
+	console.log("inside second text")
+	$("#second-read-more-text").slideUp();
 		$("#second-read-more-button").show();
 	    $("#second-read-less-button").hide(); 
+	    event.preventDefault();
+}	    
 
 
 // // If you click read more on second <p>, slide down text with a "read less" link
